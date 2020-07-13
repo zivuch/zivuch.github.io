5b33ad57f2098e33d334ab39ff1cbaf77f250675
zivuch <ziv@developers.institute>
2020-07-13 11:22:28 +0300

    Add files via upload

--- ajax.js
+++ ajax.js
@@ -1,51 +1,28 @@
-// 1 step
 let xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://www.javatpoint.com/xmlpages/emails.xml');
 
-xhr.withCredentials = true;
-
+xhr.open('GET','https://zivuch.github.io/newajax/data.xml');
 xhr.responseType = 'document';
 xhr.send();
 
-xhr.onload = function(){
-  if(xhr.status != 200){
-    console.log(`Error: ${xhr.status}: ${xht.statusText}`);
+xhr.onload = function() {
+  if(xhr.status != 200) {
+    console.log(`Error: ${xhr.status}: ${xhr.statusText}`);
   }
   else{
-    console.log(xhr.response);
+    console.log(`Done, got ${xhr.response.length} bytes`); // response is the server
+    const xml = xhr.response;
+    console.log(xml.getElementsByTagName('user'));
   }
 }
 
-// xhr.onprogress = function(event) {
-//   if (event.lengthComputable) {
-//     console.log(`Received ${event.loaded} of ${event.total} bytes`);
-//   } else {
-//     console.log(`Received ${event.loaded} bytes`); // no Content-Length
-//   }
-// };
-
-xhr.onerror = function() {
-  console.log('Error something wrong###');
+xhr.onprogress = function(event) {
+  if (event.lengthComputable) {
+    console.log(`Received ${event.loaded} of ${event.total} bytes`);
+  } else {
+    console.log(`Received ${event.loaded} bytes`); // no Content-Length
+  }
 };
 
-
-const built = (arr) => {
-  const root = document.getElementById('root');
-  arr.forEach((item, i) => {
-    let div = document.createElement('div');
-    let h1 = document.createElement('h3')
-    let p = document.createElement('p');
-    let img = document.createElement('img');
-
-    h1.innerText = item.name;
-    p.innerText = item.company.name;
-    img.setAttribute('src', `https://robohash.org/${item.id}?200x200`)
-
-    div.appendChild(img);
-    div.appendChild(h1);
-    div.appendChild(p);
-
-    root.appendChild(div);
-  });
-
-}
+xhr.onerror = function() {
+  alert("Request failed");
+};
